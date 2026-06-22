4c6d8d2162dd3d962f0b9f43a7077be9c590d47c
Archie Healy <archiethehead@gmail.com>
2026-06-22 11:26:28 +0100

    Player
    Chore: Integrate a Visual Studio solution (.sln) to handle advanced development for .NET
    
    Feat: Add a player class with basic input functionality for moving the character and camera, controlled by a C# script.

---
+++ Scripts/Player.cs
@@ -0,0 +1,69 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D {
+    public const float Speed = 5.0f;
+    public const float JumpVelocity = 4.5f;
+    public const float Sensitivity = 0.003f;
+
+    Camera3D Camera = null;
+
+    public override void _Ready() {
+
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        Camera = GetNode<Camera3D>("Camera");
+
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        Vector3 velocity = Velocity;
+
+        // Add the gravity.
+        if (!IsOnFloor()) {
+            velocity += GetGravity() * (float)delta;
+        }
+
+        // Handle Jump.
+        if (Input.IsActionJustPressed("Spacebar") && IsOnFloor()) {
+            velocity.Y = JumpVelocity;
+        }
+
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 inputDir = Input.GetVector("A", "D", "W", "S");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        if (direction != Vector3.Zero) {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+    public override void _UnhandledInput(InputEvent @Event) {
+
+        if (@Event is InputEventMouseMotion) {
+
+            InputEventMouseMotion MouseMotionEvent = @Event as InputEventMouseMotion;
+
+            Vector2 RelativePosition = MouseMotionEvent.GetRelative();
+            float RelativeX = RelativePosition.X * -1;
+            float RelativeY = RelativePosition.Y * -1;
+
+            RotateY(RelativeX * Sensitivity);
+            Camera.RotateX(RelativeY * Sensitivity);
+
+            Vector3 newRotation = Camera.GetRotation();
+            newRotation.X = Math.Clamp(Camera.Rotation.X, Mathf.DegToRad(-60), Mathf.DegToRad(70));
+            Camera.SetRotation(newRotation);
+
+        }
+
+    }
+
+}
