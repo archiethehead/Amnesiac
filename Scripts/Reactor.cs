f908dfa4137ec565d12fb7ca143347f4012275fb
Archie Healy <archiethehead@gmail.com>
2026-06-25 17:31:01 +0100

    Reactor Not Interactable
    Fix: The Reactor, in preperation for it's 3D model integration, is no longer an interactable as per the demo specification.

--- Scripts/Reactor.cs
+++ Scripts/Reactor.cs
@@ -1,8 +1,7 @@
 using Godot;
 using System;
-using System.Security.Cryptography.X509Certificates;
 
-public partial class Reactor : StaticBody3D, Employee, Interactable {
+public partial class Reactor : Node3D, Employee {
 
     const float TemperatureIncreaseRate = 50.0f;
     public bool IsExploded;
@@ -11,15 +10,8 @@ public partial class Reactor : StaticBody3D, Employee, Interactable {
     [Export] public float MaxTemperature = 0.0f;
     [Export] GameManager GameManager = null;
     [Export] AreaLight3D Light = null;
-    [Export] Label3D InteractLabel = null;
 
     public override void _Process(double delta) {
-
-        if (InteractLabel.Visible == true) {
-
-            Employee.CallManagerWithSelf(GameManager, InteractLabel, "FacePlayer");
-
-        }
 
         Temperature += TemperatureIncreaseRate * (float)delta;
 
@@ -50,34 +42,11 @@ public partial class Reactor : StaticBody3D, Employee, Interactable {
 
     }
 
-    public void Interact() {
-
-        GD.Print("You interacted with me!");
-
-    }
-
-    public void ShowInteract() {
-
-        if (!IsExploded) {
-
-            InteractLabel.Visible = true;
-
-        }
-    
-    }
-
-    public void HideInteract() {
-
-        InteractLabel.Visible = false;
-
-    }
-
     public void ReactorExplode() {
 
         IsExploded = true;
         Light.LightColor = (new Color(0, 0, 0));
         this.SetProcess(false);
-        HideInteract();
         GD.Print("BANG!");
 
     }
