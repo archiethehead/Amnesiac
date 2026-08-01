0fc5da55be92e7156065240475867c35543e8690
Archie Healy <archiethehead@gmail.com>
2026-08-01 18:19:59 +0100

    inst
    Feat: Implement demo commands to the console, being a temp 'help'
    command, and a command to spawn tools called 'inst.'

--- Scripts/Autoloads/Console.cs
+++ Scripts/Autoloads/Console.cs
@@ -1,11 +1,16 @@
 using Godot;
+using Godot.Collections;
 using System;
+using System.Collections.Generic;
 
 public partial class Console : CanvasLayer
 {
 
     private GameManager GameManager = null;
+    private string[] Args = null;
     [Export] LineEdit LineEdit = null;
+    private System.Collections.Generic.Dictionary<string, Action> CommandDict = new System.Collections.Generic.Dictionary<string, Action>();
+    private bool FirstFrame = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -17,9 +22,14 @@ public partial class Console : CanvasLayer
         LineEdit.KeepEditingOnTextSubmit = true;
         this.ProcessMode = ProcessModeEnum.Disabled;
 
+        CommandDict.Add("help", Help);
+        CommandDict.Add("inst", Inst);
+
     }
 
     public override void _Process(double delta) {
+
+        if (FirstFrame) LineEdit.Clear(); FirstFrame = false;
 
         if (Input.IsActionPressed(InputMap.Pause)) {
 
@@ -29,6 +39,16 @@ public partial class Console : CanvasLayer
         }
 
         else if (Input.IsActionPressed(InputMap.Submit)) {
+
+            string RawCommand = LineEdit.Text;
+            string[] args = RawCommand.Split(' ');
+
+            if (args.Length == 0) return;
+            args[0] = args[0].ToLower();
+            Args = args;
+
+            if (!CommandDict.ContainsKey(args[0])) return;
+            CommandDict[args[0]]();
 
             LineEdit.Clear();
 
@@ -51,6 +71,46 @@ public partial class Console : CanvasLayer
         Input.MouseMode = GameManager.MouseMode;
         Hide();
         this.ProcessMode = ProcessModeEnum.Disabled;
+        FirstFrame = true;
+
+    }
+
+    // Commands
+
+    public void Help() {
+
+        GD.Print("test");
+    
+    }
+
+    public void Inst() {
+
+        string FilePath = "res://Objects/Tools/" + Args[1] + ".tscn";
+        int loop = 1;
+
+        if (Args.Length > 2) {
+
+            loop = Args[2].ToInt();
+        
+        }
+
+        PackedScene Tool = GD.Load<PackedScene>(FilePath);
+
+        if (Tool == null) return;
+
+
+        for (int i = 0; i < loop; i++) {
+
+            Node Instance = Tool.Instantiate();
+            GetTree().Root.AddChild(Instance);
+
+            if (Instance is Node3D n) {
+
+                n.GlobalPosition = GameManager.Player.GlobalPosition;
+
+            }
+
+        }
 
     }
 	
